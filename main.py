e36f23b2ae5d09055f689f1d078527dde19fe231
AaronVelez <AI.VelezRamirez@gmail.com>
2020-11-10 22:06:15 -0600

    Update main.py
    Created imports, pin definitions and main loop headers

--- main.py
+++ main.py
@@ -13,3 +13,21 @@
 # Temperature setpoint is encoded in an analog signal from LI6800 console
 # Fan speed is read from fan tachometer
 # Fan speed is encoded in an analog singal and sent to LI6800
+
+
+# Module imports
+import board
+import digitalio
+import analogio
+import time
+
+
+
+# Set pin definitions
+LED_Temp = AnalogIn(board.A1)
+
+
+
+# Main Loop
+
+
