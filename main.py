d23ca2e6f649d1e17c09624c4d0831eab3ca8fe2
AaronVelez <AI.VelezRamirez@gmail.com>
2020-11-10 21:50:07 -0600

    Update main.py
    I added a header

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-# Here goes the CODE, START WRITTING HERE:
-  
-  
-  
+# Name: main.py
+# Created: 10/11/2020
+# Authors: Juan de Dios Moreno González y Aarón I. Vélez Ramírez
+# Universidad Nacional Autónoma de México
+
+# Code for temperature control of LI6800 custum LED lamp
+
+# Details:
+# Runs in an Arduino-compatible Trinket board with an ARM Cortex M0+ processor
+# It measures LED temperature using a thermistor
+# It controls a 4-wire fan via a 25kHz PWM signal
+# PWM duty cycle is set by a PID algorithm to control LED temperaure
+# Temperature setpoint is encoded in an analog signal from LI6800 console
+# Fan speed is read from fan tachometer
+# Fan speed is encoded in an analog singal and sent to LI6800
