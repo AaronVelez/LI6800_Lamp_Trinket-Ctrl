d3562ff924426bfcad2f51b37eecc8f5f38297b6
Aarón I. Vélez Ramírez <aaron.velez@comunidad.unam.mx>
2020-11-16 21:40:04 -0600

    Add print time for debugging

--- main.py
+++ main.py
@@ -235,4 +235,5 @@ while True:
     # 5.1 Write to Fan_PWM_pin the new PWM duty cycle in 16-bit format (Fan_PWM_duty_cycle_bits) calculated by the PID algorithm
     Fan_PWM_pin.duty_cycle = Fan_PWM_duty_cycle_bits
 
+    print(time.monotonic())
     time.sleep(1)
