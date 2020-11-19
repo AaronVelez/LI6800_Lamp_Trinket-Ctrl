84b1e62c2fd02ea377d24f9958e34d388e0d8d21
Aarón I. Vélez Ramírez <aaron.velez@comunidad.unam.mx>
2020-11-19 12:16:17 -0600

    Firmware 1.0

--- main.py
+++ main.py
@@ -3,11 +3,13 @@
 # Authors: Juan de Dios Moreno González y Aarón I. Vélez Ramírez
 # Universidad Nacional Autónoma de México
 #
+# Firmware version: 1.0
+# 
 # Code for temperature control of LI6800 custum LED lamp
 #
 # Details:
 # Runs in an Adafruit Trinket board with an ARM Cortex M0+ processor
-# It measures LED temperature using a thermistor
+# It measures LED temperature using a thermistor and voltage divider
 # It controls a 4-wire fan via a 25kHz PWM signal
 # PWM duty cycle is set by a PID algorithm to control LED temperaure
 # Temperature setpoint is encoded in an analog signal from LI6800 console
@@ -97,7 +99,7 @@ LED_Temp_Cdeg =0.0
 
 
 ##### Functions
-# PID function attributed to secction "4.5 Realizable PID Control" https://github.com/jckantor/CBE30338
+# PID function attributed to https://github.com/jckantor/CBE30338
 def PID(Kp, Ki, Kd, MV_bar=0, MV_min=0, MV_max=65535, beta=1, gamma=0, N=5):
     # initialize stored data
     t_prev = -100
@@ -116,29 +118,29 @@ def PID(Kp, Ki, Kd, MV_bar=0, MV_min=0, MV_max=65535, beta=1, gamma=0, N=5):
         # see if a tracking data is being supplied
         if len(data) < 4:
             t, SP, PV = data
-            print("No TR")
+            # print("No TR")
         else:
             t, SP, PV, TR = data
             I = TR - MV_bar - P - D
-            print ('TR: {}'.format(TR))
-            print ('I_start: {}'.format(I))
+            # print ('TR: {}'.format(TR))
+            # print ('I_start: {}'.format(I))
 
         # PID calculations
-        print("PID calculation start")
-        print ('MVmax: {}'.format(MV_max))
+        # print("PID calculation start")
+        # print ('MVmax: {}'.format(MV_max))
         P = Kp*(beta*SP - PV)
-        print ('P: {}'.format(P))
+        # print ('P: {}'.format(P))
         I = I + Ki*(SP - PV)*(t - t_prev)
-        print ('I: {}'.format(I))
+        # print ('I: {}'.format(I))
         eD = gamma*SP - PV
         D = N*Kp*(Kd*eD - S)/(Kd + N*Kp*(t - t_prev))
-        print ('D: {}'.format(D))
+        # print ('D: {}'.format(D))
         MV = MV_bar + P + I + D
-        print ('MVraw: {}'.format(MV))
+        # print ('MVraw: {}'.format(MV))
 
         # Constrain MV to range MV_min to MV_max for anti-reset windup
         MV = MV_min if MV < MV_min else MV_max if MV > MV_max else MV
-        print ('MV: {}'.format(MV))
+        # print ('MV: {}'.format(MV))
         I = MV - MV_bar - P - D
 
         # update stored data for next iteration
@@ -158,34 +160,34 @@ PID_fan.send(None)
 ##### It runs forever
 while True:
     # Step 1. Read LED temperature setpoin defined by LI6800.
-    print("\n")
-    print("Step 1 start")
+    # print("\n")
+    # print("Step 1 start")
     # 1.1 Read n times the analog voltage proportional to desired LED temperature at LED_Temp_Ctrl_pin
     sum = 0
     for x in range(n):
         sum += LED_Temp_Ctrl_pin.value
     mean = sum / n
     LED_Temp_Ctrl_voltage = (mean * Ref_voltage)/65535
-    print('LED Temp Ctrl voltage: {} volts'.format(LED_Temp_Ctrl_voltage))
+    # print('LED Temp Ctrl voltage: {} volts'.format(LED_Temp_Ctrl_voltage))
 
     # 1.2 Convert it to °C using constant (needs to be added to constants) and store it in LED_Temp_Ctrl_Cdeg
     LED_Temp_Ctrl_Cdeg = Min_LED_Temp + (LED_Temp_Ctrl_voltage * ((Max_LED_Temp-Min_LED_Temp)/3.3))
-    print('LED Temp Ctrl Cdeg: {} °C'.format(LED_Temp_Ctrl_Cdeg))
+    # print('LED Temp Ctrl Cdeg: {} °C'.format(LED_Temp_Ctrl_Cdeg))
 
 
     # Step 2. Read LED temperature
-    print("Step 2 start")
+    # print("Step 2 start")
     # 2.1 Read analog voltage at LED_Temp_pin and store it in LED_Temp_voltage
     sum = 0
     for x in range(n):
         sum += LED_Temp_pin.value
     mean = sum / n
     LED_Temp_voltage = (mean * Ref_voltage)/65535
-    print('LED Temp voltage: {} volts'.format(LED_Temp_voltage))
+    # print('LED Temp voltage: {} volts'.format(LED_Temp_voltage))
 
     # 2.2 Translate read voltage to Thermistor resistance
     Thermistor_R = (LED_Temp_voltage*Div_R)/(Ref_voltage - LED_Temp_voltage)
-    print('Thermistor resistance: {} kohms'.format(Thermistor_R))                  ## Borrar este print envetualmente.
+    # print('Thermistor resistance: {} kohms'.format(Thermistor_R))                  ## Borrar este # print envetualmente.
 
     # 2.3 Translate thermistor resistance to LED temperature and store it in LED_Temp_Cdeg
     if Thermistor_R/Nom_R <= 68.6 and Thermistor_R/Nom_R > 3.274:         # -50 to 0 °C range
@@ -208,13 +210,13 @@ while True:
                          (d[1]*math.log(Thermistor_R/Nom_R)) +
                          ((d[2]*math.log(Thermistor_R/Nom_R))**2) +
                          ((d[3]*math.log(Thermistor_R/Nom_R))**3) )) - 273.15
-    print ('Temperature: {} °C'.format(LED_Temp_Cdeg))                            ## Borrar este print envetualmente.
-    print((LED_Temp_Cdeg,))
+    # print ('Temperature: {} °C'.format(LED_Temp_Cdeg))                            ## Borrar este # print envetualmente.
+    # print((LED_Temp_Cdeg,))
     time.sleep(0.1)
 
 
     # Step 3. Read fan speed and send it to LI6800
-    print("Step 3 start")
+    # print("Step 3 start")
     # 3.1 Read digital pulses at Fan_Tach_pin
     ########################################################################
     hall_count = 0
@@ -233,12 +235,12 @@ while True:
 
     # 3.2 Calculate fan speed in rpm and store it in Fan_Tach_rpm
     Fan_Tach_rpm = ((hall_count / (end - start)) / rpm_hall) * 60
-    print ('Fan speed: {} rpm'.format(Fan_Tach_rpm))
+    # print ('Fan speed: {} rpm'.format(Fan_Tach_rpm))
 
     # 3.3 Convert Fan_Tach_rpm into a 16-bit value, mapping the maximum fan speed (Fan_max_rpm constant) to 65,536,
     # and store it in Fan_Tach_bits
     Fan_Tach_bits = (Fan_Tach_rpm * 65535) / Fan_max_rpm
-    print ('Fan speed: {} bits'.format(Fan_Tach_bits))
+    # print ('Fan speed: {} bits'.format(Fan_Tach_bits))
 
     # 3.4 Output FanTach_bits value to Fan_Speed_pin, so LI6800 can read it
     # AnalogOut.value acepts 16-bit values; so Fan_Tach_bits is mapped from 0 to 3.3 V by board
@@ -247,7 +249,7 @@ while True:
 
 
     # Step 4. Run PID control algorithm.
-    print("Step 4 start")
+    # print("Step 4 start")
     # It calculates the required fan speed (in PWM duty cycle values IN 16-bit format) to achive the desired LED temperature.
     # It takes into consideration the current and near past LED temperature.
     t = time.monotonic()
@@ -258,12 +260,11 @@ while True:
     # Given time (t) process variable (PV), setpoint (SP) and tracked MV (TR), it returns manipulated variable (MV) and P, I, D and d
     MV = PID_fan.send([t, PV, SP])
     Fan_PWM_duty_cycle_bits = MV
-    print ('PID PWM control: {} %'.format(Fan_PWM_duty_cycle_bits*100/65535))
+    # print ('PID PWM control: {} %'.format(Fan_PWM_duty_cycle_bits*100/65535))
 
 
     # Step 5. Set new Fan speed
     # 5.1 Write to Fan_PWM_pin the new PWM duty cycle in 16-bit format (Fan_PWM_duty_cycle_bits) calculated by the PID algorithm
     Fan_PWM_pin.duty_cycle = int(round(Fan_PWM_duty_cycle_bits))
 
-    print('Time: {}'.format(time.monotonic()))
-    #time.sleep(5)
+    # print('Time: {}'.format(time.monotonic()))
